a772aba7d266e52c5d5b18006ab878af78ecd311
fabiannabil1 <fabiannabil00@gmail.com>
2024-11-29 14:23:48 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
@@ -11,29 +12,27 @@ use App\Http\Controllers\Seller\SellerController;
 use App\Http\Controllers\Admin\UserManagementController;
 
 Route::get('/', function () {
-    return view('info-product');
+    return view('landing');
 });
-
-// Route::get('/', function () {
-//     return view('test');
-// });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::get('admin/user-management', [UserManagementController::class, 'index'])->name('admin.userManagement');
-    Route::get('admin/product-management', [ProductController::class, 'index'])->name('admin.productManagement');
+    Route::delete('admin/user-management/delete/{user_id}', [UserManagementController::class, 'deleteUser'])->name('admin.delete-user');
+    Route::get('admin/kategori', [CategoryController::class, 'index'])->name('admin.kategori');
+    Route::put('admin/edit-kategori', [CategoryController::class, 'edit'])->name('admin.edit-kategori');
+    Route::get('admin/product', [ProductController::class, 'index'])->name('admin.view-product');
 });
 
 Route::middleware(['auth', 'role:seller'])->group(function () {
     Route::get('seller/dashboard', [SellerController::class, 'index'])->name('seller.dashboard');
-    Route::get('seller/product', [ProductController::class, 'product'])->name('seller.product');
-    Route::get('seller/product/create', [ProductController::class, 'addProduct'])->name('seller.product.create');
+    Route::get('seller/product', [ProductController::class, 'addProduct'])->name('seller.product');
+    Route::post('seller/product/create', [ProductController::class, 'store'])->name('seller.add-product');
 });
 
 Route::middleware(['auth', 'role:user'])->group(function () {
     Route::get('user/dashboard', [UserController::class, 'index'])->name('user.dashboard');
 });
-
 
 
 Route::get('optimize', function () {
