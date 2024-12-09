d16b8b2e9c9877c820db5838d53c00520bbda279
fabiannabil1 <fabiannabil00@gmail.com>
2024-12-09 12:09:00 +0700

    fix : dupe routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\AddressController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\User\CartController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\User\OrderController;
@@ -28,15 +25,10 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/kategori', [CategoryController::class, 'index'])->name('admin.kategori');
     Route::put('admin/edit-kategori', [CategoryController::class, 'edit'])->name('admin.edit-kategori');
     Route::get('admin/product', [ProductController::class, 'index'])->name('admin.view-product');
-    Route::delete('admin/user-management/delete/{user_id}', [UserManagementController::class, 'deleteUser'])->name('admin.delete-user');
-    Route::get('admin/kategori', [CategoryController::class, 'index'])->name('admin.kategori');
-    Route::put('admin/edit-kategori', [CategoryController::class, 'edit'])->name('admin.edit-kategori');
-    Route::get('admin/product', [ProductController::class, 'index'])->name('admin.view-product');
     Route::get('admin/category', [CategoryController::class, 'index'])->name('admin.view-category');
     Route::get('admin/search-product', [ProductController::class, 'search'])->name('admin.search-product');
     Route::delete('admin/product/delete/{id}', [ProductController::class, 'delete'])->name('admin.delete-product');
-    // Route::delete('admin/category/delete/{id}', [CategoryController::class, 'delete'])->name('admin.delete-category');
-    Route::delete('admin/category/delete/{id}', [CategoryController::class,'delete'])->name('admin.delete-category');
+    Route::delete('admin/category/delete/{id}', [CategoryController::class, 'delete'])->name('admin.delete-category');
     Route::put('admin/product/edit/{id}', [ProductController::class, 'edit'])->name('admin.edit-product');
     Route::post('admin/product/category', [CategoryController::class, 'store'])->name('admin.add-category');
 });
@@ -55,12 +47,9 @@ Route::middleware(['auth', 'role:user'])->group(function () {
     Route::delete('user/cart/remove/{id}', [CartController::class, 'remove'])->name('user.cart.remove');
     Route::post('user/cart/checkout', [CartController::class, 'checkout'])->name('user.cart.checkout');
     Route::post('user/order/payment', [OrderController::class, 'store'])->name('user.order.payment');
-
     Route::post('user/transaction/notification', [TransactionController::class, 'notificationHandler'])->name('user.transaction.notification');
     Route::get('user/address/add', [AddressController::class, 'index'])->name('user.add-address');
     Route::post('user/address/save', [AddressController::class, 'saveAddress'])->name('user.address.store');
-
-    Route::get('user/address/add', [AddressController::class, 'index'])->name('user.add-address');
     Route::get('/get-cities/{provinceId}', [AddressController::class, 'getCities']);
     Route::get('/get-districts/{cityId}', [AddressController::class, 'getDistricts']);
     Route::get('/get-villages/{districtId}', [AddressController::class, 'getVillages']);
@@ -69,9 +58,7 @@ Route::middleware(['auth', 'role:user'])->group(function () {
     Route::get('payment/success', [PaymentController::class, 'success'])->name('payment.success');
     Route::get('payment/pending', [PaymentController::class, 'pending'])->name('payment.pending');
     Route::get('payment/error', [PaymentController::class, 'error'])->name('payment.error');
-
 });
-
 
 Route::get('optimize', function () {
     Artisan::call('cache:clear');
